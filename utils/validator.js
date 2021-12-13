d5dfd82de7331df962377ac2e96cec9caf4e5079
anducnguyen <58422724+ducan3007@users.noreply.github.com>
2021-12-13 11:23:08 +0700

    d

--- utils/validator.js
+++ utils/validator.js
@@ -9,14 +9,13 @@ module.exports.validatorUser = [
     .notEmpty()
     .withMessage('Username cannot be blank')
 
-    .isLength({ min: 5 })
-    .withMessage('User name must be at least 5 characters long')
+    .isLength({ min: 1 })
 
     .isLength({ max: 16 })
     .withMessage('Username must be at most 16 characters long')
 
     .matches(/^[a-zA-Z0-9_-]+$/)
-    .withMessage('User name contains invalid characters'),
+    .withMessage('Username contains invalid characters'),
 
     check('password')
     .exists()
@@ -26,8 +25,7 @@ module.exports.validatorUser = [
     .notEmpty()
     .withMessage('Password cannot be blank')
 
-    .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters long')
+    .isLength({ min: 1 })
 
     .isLength({ max: 50 })
     .withMessage('Password must be at most 50 characters long')
@@ -43,8 +41,7 @@ module.exports.validatorPost = [
     .notEmpty()
     .withMessage('Post title cannot be blank')
 
-    .isLength({ min: 10 })
-    .withMessage('Post title must be at least 10 characters long')
+    .isLength({ min: 1 })
     .isLength({ max: 200 })
     .withMessage('Post title must be at most 200 characters long'),
     check('body')
@@ -55,7 +52,7 @@ module.exports.validatorPost = [
     .notEmpty()
     .withMessage('Please enter valid body !')
 
-    .isLength({ min: 10 })
+    .isLength({ min: 1 })
     .withMessage('Body must be at least 10 characters long')
     .isLength({ max: 1500 })
     .withMessage('Body is at most 1500 character long'),
@@ -63,16 +60,15 @@ module.exports.validatorPost = [
     check('tagname')
     .exists()
     .trim()
-    .withMessage('Please enter valid tagname !')
+    .withMessage('Please enter valid tagname!')
 
     .notEmpty()
-    .withMessage('Please enter valid tagname !')
+    .withMessage('Please enter valid tagname!')
     .toLowerCase()
 
     .isLength({ max: 50 })
     .withMessage('Tagname is too long')
     .matches(/^[a-zA-Z0-9#++.-]+([^,~!@$%^&*()_=/\\ ]*,[^,@#/\\. ][a-zA-Z0-9#++.-]*){0,5}$/)
-    // .matches(/^[a-zA-Z0-9#++]+([^, ]*,[^, ]*){0,5}$/)
     .withMessage("Please enter valid tagname !")
 
 ];
@@ -85,8 +81,7 @@ module.exports.validatorAnswers = [
     .notEmpty()
     .withMessage('Answers cannot be blank')
 
-    .isLength({ min: 5 })
-    .withMessage('answers must be at least 15 characters long')
+    .isLength({ min: 1 })
 
     .isLength({ max: 4000 })
     .withMessage('Answers is too long'),
@@ -100,9 +95,9 @@ module.exports.validatorComments = [
     .notEmpty()
     .withMessage('comment cannot be blank')
 
-    .isLength({ min: 5 })
-    .withMessage('commnet must be at least 5 characters long')
+    .isLength({ min: 1 })
+    .withMessage('comment invalid')
 
     .isLength({ max: 200 })
-    .withMessage('comment too long'),
+    .withMessage('comment is too long'),
 ];
