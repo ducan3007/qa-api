492d20e56a51217a6296f7333b7807d43a92eb8f
Sotatek-AnNguyen5 <an.nguyen5@sotatek.com>
2022-10-09 02:15:21 +0700

    d;

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-const morgan = require('morgan');
-const helmet = require('helmet');
-const mongoose = require('mongoose')
-const cors = require('cors');
-const compression = require('compression');
-const path = require('path');
-const http = require('http');
-const express = require('express');
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const compression = require("compression");
+const path = require("path");
+const http = require("http");
+const express = require("express");
 
-const routers = require('./routes/routes');
+const routers = require("./routes/routes");
 
-
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 
-const urlDbConnect = process.env.db
-mongoose.connect(urlDbConnect, { useNewUrlParser: true, useUnifiedTopology: true }, error => {
+const urlDbConnect = process.env.db;
+mongoose.connect(
+  urlDbConnect,
+  { useNewUrlParser: true, useUnifiedTopology: true, dbName: process.env.DB_NAME },
+  (error) => {
     if (error) {
-        throw error;
+      throw error;
     }
-    console.log("Mongodb connected!")
-})
+    console.log("Mongodb connected!");
+  }
+);
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(compression());
 
 // app.options('*', cors());
 // app.use(cors({ origin: '*' }));
-app.use(cors())
+app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api', routers);
+app.use("/api", routers);
 
 const PORT = process.env.PORT || 5000;
 
 // const server = http.createServer(app);
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
